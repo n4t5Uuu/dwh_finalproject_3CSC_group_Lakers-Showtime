14a31afc93de46277c7a8ab465974e3e3034bbe3
Ritsuuu1 <jpbsantamaria23@gmail.com>
2025-12-19 12:08:39 +0800

    dag final

--- dags/dag_master_pipeline.py
+++ dags/dag_master_pipeline.py
@@ -11,7 +11,6 @@ from psycopg2.extras import execute_values
 sys.path.append("/scripts")
 
 # DIRECTORY SETUP
-from setup_directories import main as setup_directories
 from convert_all_to_csv import main as convert_to_csv
 
 # BUSINESS
@@ -524,7 +523,15 @@ with DAG(
                     ELSE COALESCE(dc.campaign_key, 0)
                 END AS campaign_key,
 
-                COALESCE(tc.availed, 0) AS availed_flag,
+                CASE
+                    WHEN (
+                        CASE
+                            WHEN NULLIF(NULLIF(tc.campaign_id, ''), 'NaN') IS NULL THEN -1
+                            ELSE COALESCE(dc.campaign_key, 0)
+                        END
+                    ) IN (-1, 0) THEN 0
+                    ELSE COALESCE(tc.availed, 0)
+                END AS availed_flag,
 
                 dc.discount_pct,
 
@@ -720,146 +727,9 @@ with DAG(
                 """
             )
 
-        create_fact_campaign = PostgresOperator(
-            task_id="create_fact_campaign_availed",
-            postgres_conn_id="postgres_default",
-            sql="""
-            CREATE SCHEMA IF NOT EXISTS shopzada;
-
-            CREATE TABLE IF NOT EXISTS shopzada.fact_campaign_availed (
-                fact_campaign_key SERIAL PRIMARY KEY,
-
-                order_id VARCHAR(100) NOT NULL,
-
-                campaign_key INT NOT NULL,
-                user_key INT NOT NULL,
-                merchant_key INT NOT NULL,
-                staff_key INT NOT NULL,
-                date_key INT NOT NULL,
-
-                availed INT NOT NULL,
-                discount_pct INT,
-
-                CONSTRAINT uq_fact_campaign UNIQUE (order_id, campaign_key, date_key)
-            );
-            """
-        )
-
-        load_fact_campaign = PostgresOperator(
-            task_id="load_fact_campaign_availed",
-            postgres_conn_id="postgres_default",
-            sql="""
-            TRUNCATE shopzada.fact_campaign_availed;
-
-            INSERT INTO shopzada.fact_campaign_availed (
-                order_id,
-                campaign_key,
-                user_key,
-                merchant_key,
-                staff_key,
-                date_key,
-                availed,
-                discount_pct
-            )
-            SELECT
-                tc.order_id,
-                
-                CASE
-                    WHEN NULLIF(NULLIF(tc.campaign_id, ''), 'NaN') IS NULL THEN -1 -- Not Applicable
-                    ELSE COALESCE(dc.campaign_key, 0)        -- Unknown (late-arriving)
-                END AS campaign_key,
-
-                COALESCE(du.user_key, 0)      AS user_key,
-                COALESCE(dm.merchant_key, 0)  AS merchant_key,
-                COALESCE(ds.staff_key, 0)     AS staff_key,
-                tc.date_key,
-                tc.availed,
-                dc.discount_pct
-            FROM staging.transactional_campaign_clean tc
-
-            -- CAMPAIGN DIM 
-            LEFT JOIN shopzada.dim_campaign dc
-                ON tc.campaign_id = dc.campaign_id
-
-            -- ORDER -> USER
-            LEFT JOIN staging.orders_clean o
-                ON tc.order_id = o.order_id
-
-            LEFT JOIN LATERAL (
-                SELECT du.user_key
-                FROM shopzada.dim_user du
-                WHERE du.user_id = o.user_id
-                AND (
-                        -- normal SCD2 match
-                        TO_DATE(tc.date_key::text, 'YYYYMMDD')
-                            BETWEEN du.effective_from
-                                AND COALESCE(du.effective_to, DATE '9999-12-31')
-
-                        -- fallback: transaction before first version
-                    OR TO_DATE(tc.date_key::text, 'YYYYMMDD') <
-                        (
-                            SELECT MIN(effective_from)
-                            FROM shopzada.dim_user
-                            WHERE user_id = o.user_id
-                        )
-                )
-                ORDER BY du.effective_from
-                LIMIT 1
-            ) du ON TRUE
-
-
-
-            -- ORDER -> MERCHANT / STAFF
-            LEFT JOIN staging.order_with_merchant_clean om
-                ON tc.order_id = om.order_id
-
-            LEFT JOIN LATERAL (
-                SELECT dm.merchant_key
-                FROM shopzada.dim_merchant dm
-                WHERE dm.merchant_id = om.merchant_id
-                AND (
-                        TO_DATE(tc.date_key::text, 'YYYYMMDD')
-                            BETWEEN dm.effective_from
-                                AND COALESCE(dm.effective_to, DATE '9999-12-31')
-
-                    OR TO_DATE(tc.date_key::text, 'YYYYMMDD') <
-                        (
-                            SELECT MIN(effective_from)
-                            FROM shopzada.dim_merchant
-                            WHERE merchant_id = om.merchant_id
-                        )
-                )
-                ORDER BY dm.effective_from
-                LIMIT 1
-            ) dm ON TRUE
-
-
-            LEFT JOIN LATERAL (
-                SELECT ds.staff_key
-                FROM shopzada.dim_staff ds
-                WHERE ds.staff_id = om.staff_id
-                AND (
-                        TO_DATE(tc.date_key::text, 'YYYYMMDD')
-                            BETWEEN ds.effective_from
-                                AND COALESCE(ds.effective_to, DATE '9999-12-31')
-
-                    OR TO_DATE(tc.date_key::text, 'YYYYMMDD') <
-                        (
-                            SELECT MIN(effective_from)
-                            FROM shopzada.dim_staff
-                            WHERE staff_id = om.staff_id
-                        )
-                )
-                ORDER BY ds.effective_from
-                LIMIT 1
-            ) ds ON TRUE
-
-            """
-        )
+    
 
         create_fact_orders >> load_fact_orders
-        create_fact_table >> load_fact_table 
-        create_fact_campaign >> load_fact_campaign 
-        
+        create_fact_table >> load_fact_table     
 
     clean_group >> stage_group >> dim_group >> fact_group
